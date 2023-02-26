26a1d33d56333b6e88ecf124b0ea079a3b109220
andrey <xxxkeep3rxxx@gmail.com>
2023-02-26 23:29:13 +0300

    add scss

--- resources/views/user/registration.blade.php
+++ resources/views/user/registration.blade.php
@@ -9,14 +9,23 @@
     НижБлог/Регистрация
 @endsection
 @section('content')
-<button type="button" class="btn btn-primary">Test Bootstrap</button>
-    <form method="post" name="reg_form">
-        <input type="text" name="name" placeholder="Имя">
-        <input type="text" name="surname" placeholder="Фамилия">
-        <input type="text" name="patronymic" placeholder="Отчество">
-        <input type="text" name="email" placeholder="Email">
-        <input type="text" name="login" placeholder="Логин">
-        <input type="password" name="password" placeholder="Пароль">
-        <input type="password" name="password_confirmation" placeholder="Повторите пароль">
-    </form>
+    <div id="image-background">
+        <div class="filter-background d-flex">
+            <div class="auth-form-block justify-content-center align-items-center">
+                <div class="h1">Добро пожаловать!</div>
+                <hr/>
+                <form class="d-block">
+                    <input placeholder="Логин">
+                    <input type="password" placeholder="Пароль">
+                    <a href="">Забыли пароль?</a>
+                    <input type="checkbox"> <span>Запомнить меня</span>
+                    <a href="">
+                        <img src="{{asset('/img/vk-logo.png')}}">
+                        <div>Войти с помощью ВК</div>
+                    </a>
+                    <input type="submit" value="Войти">
+                </form>
+            <div>
+        <div>
+    </div>
 @endsection
